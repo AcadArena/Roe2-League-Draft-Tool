cf2a3cab39b32473431c8f4f80895fcc0de80f48
Jasper Concepcion <jasper@manok.dev>
2022-06-13 07:51:44 +0800

    lint fixes

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -10,7 +10,5 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    quotes: ["error", "single"],
-    "object-curly-spacing": ["error", "always"],
   },
 }
